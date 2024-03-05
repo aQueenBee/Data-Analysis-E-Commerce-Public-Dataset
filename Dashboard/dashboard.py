9f6ca0a966808d65c8545b0ee04314132758ae77
Ramadhan <ramadhanalmuktafi@gmail.com>
2024-03-06 01:23:21 +0700

    tes

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -4,37 +4,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-# URL dataset
-url_main_data = "https://raw.githubusercontent.com/aQueenBee/Data-Analysis-E-Commerce-Public-Dataset/Data-Analysis-E-Commerce-Public-Dataset/Dashboard/main_data.csv"
-order_items = pd.read_csv(url_main_data)
-
-# Mengambil 100 sampel produk secara acak
-sampled_products = average_scores_per_product.sample(n=100, random_state=42)
-
-# Plot bar plot dari sampel data tanpa label indeks produk
-plt.figure(figsize=(12, 6))
-plt.bar(range(len(sampled_products)), sampled_products.values, color='skyblue')
-plt.xlabel('Produk')  # Memberikan label kosong pada sumbu x
-plt.ylabel('Rata-rata Skor Ulasan')
-plt.title('Rata-rata Skor Ulasan untuk 100 Produk Pertama (Sampel)')
-plt.xticks([])  # Menghapus label sumbu x
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.tight_layout()
-st.pyplot()
+# Unduh data CSV dari URL
+url_order_items = "https://raw.githubusercontent.com/aQueenBee/Data-Analysis-E-Commerce-Public-Dataset/Data-Analysis-E-Commerce-Public-Dataset/data/order_items_dataset.csv"
+order_items = pd.read_csv(url_order_items)
+# URL dataset order reviews
+url_order_reviews = "https://raw.githubusercontent.com/aQueenBee/Data-Analysis-E-Commerce-Public-Dataset/Data-Analysis-E-Commerce-Public-Dataset/data/order_reviews_dataset.csv"
+order_reviews = pd.read_csv(url_order_reviews)
 
 # Ambil 100 sampel produk secara acak
 sampled_products = average_scores_per_product.sample(n=100, random_state=42)
 
-# Plot grafik garis untuk setiap sampel produk
+# Plot bar plot dari sampel data
 plt.figure(figsize=(12, 6))
-for product_id in sampled_products.index:
-    # Kelompokkan data berdasarkan tahun dan produk
-    average_scores_time = merged_data[merged_data['product_id'] == product_id].groupby(merged_data['review_creation_date'].dt.year)['review_score'].mean()
-    plt.plot(average_scores_time.index, average_scores_time.values, marker='o', label=None)  # Label=None untuk menonaktifkan label produk
+plt.bar(range(len(sampled_products)), sampled_products['review_score'], color='skyblue')
+plt.xlabel('Produk')  # Memberikan label kosong pada sumbu x
+plt.ylabel('Rata-rata Skor Ulasan')
+plt.title('Rata-rata Skor Ulasan untuk 100 Sampel Produk')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.show()
 
+# Visualisasikan tren skor ulasan dari waktu ke waktu menggunakan line chart
+plt.figure(figsize=(10, 6))
+plt.plot(average_scores_time['year'], average_scores_time['review_score'], marker='o', color='b')
+plt.title('Tren Skor Ulasan dari Waktu ke Waktu')
 plt.xlabel('Tahun')
 plt.ylabel('Rata-rata Skor Ulasan')
-plt.title('Tren Skor Ulasan dari Tahun ke Tahun (100 Sampel Produk)')
 plt.grid(True)
-plt.tight_layout()
-st.pyplot()
+plt.show()
